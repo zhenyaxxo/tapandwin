8dc2ebae8f1d749b167c1963c4724e7bcc036ae7
zhenyaxxo <zhenyaxxo@gmail.com>
2020-04-26 01:06:06 +0500

    first alpha XD

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -18,7 +18,7 @@ namespace Game
 
         private int _random;
         private float nextActionTime = 1f;
-        public float period = 1f;
+        private float period = 1f;
 
         void Update()
         {
@@ -30,10 +30,37 @@ namespace Game
                 {
                     Numbers.gameObject.SetActive(false);
                     Title.text = "GO!!!";
-                    Title.transform.position = new Vector3(Title.transform.position.x + 1.25f,
+                    Title.transform.position = new Vector3(Title.transform.position.x + 1.85f,
                         Title.transform.position.y, Title.transform.position.z);
+                    StartGame();
                 }
             }
+        }
+
+        private void StartGame() // Метод вызывающий процесс игры и который проверяет правильность нажатых клавиш
+        {
+            _random = Random.Range(0, 4);
+            Debug.Log(_random);
+            switch (_random)
+            {
+                case 1:
+                    Up.gameObject.SetActive(true);
+                    break;
+                case 2:
+                    Down.gameObject.SetActive(true);
+                    break;
+                case 3:
+                    Left.gameObject.SetActive(true);
+                    break;
+                case 4:
+                    Right.gameObject.SetActive(true);
+                    break;
+            }
+        }
+
+        public void timer()
+        {
+            
         }
         
     }
