c5d4d8a2201fa607c958a67ee4acbe98164304f5
zhenyaxxo <zhenyaxxo@gmail.com>
2020-04-25 20:51:33 +0500

    first commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+namespace Game
+{
+    public class GameController : MonoBehaviour
+    {
+        public Image Up;
+        public Image Down;
+        public Image Left;
+        public Image Right;
+        public Text Numbers;
+        public Text Title;
+
+        private int _random;
+        private float nextActionTime = 1f;
+        public float period = 1f;
+
+        void Update()
+        {
+            if ((Time.time > nextActionTime) && (nextActionTime <= 3))
+            {
+                Numbers.text = nextActionTime.ToString();
+                nextActionTime += period;
+                if (Numbers.text == "3")
+                {
+                    Numbers.gameObject.SetActive(false);
+                    Title.text = "GO!!!";
+                    Title.transform.position = new Vector3(Title.transform.position.x + 1.25f,
+                        Title.transform.position.y, Title.transform.position.z);
+                }
+            }
+        }
+        
+    }
+}
---
+++ Assets/Scripts/GameProcess.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Game
+{
+    public class GameProcess: MonoBehaviour
+    {
+        public void StartGame()
+        {
+            
+        }
+
+        public int RandomKey()
+        {
+            return Random.Range(0, 4);
+        }
+    }
+}
