a3bf431d1b02d0f07f4f94e308a42914538c5510
zhenyaxxo <zhenyaxxo@gmail.com>
2020-04-26 22:57:05 +0500

    alpha test

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
@@ -16,37 +17,47 @@ namespace Game
         public Image Right;
         public Text Numbers;
         public Text Title;
-
+        public Image panel;
+        
         private int _random;
         private float nextActionTime = 1f;
         private float period = 1f;
-
-        public Text score;
-        private int _score = 0;
+        
         private float _gameTime;
         private int TimeLeft = -1;
+        public int TimeToStart = 3;
 
         public float _vertical;
         public float _horizontal;
 
-        private bool Miss;
-
+        private int _score = 0;
+        
         void Update()
         {
             _vertical = Input.GetAxis("Vertical");
             _horizontal = Input.GetAxis("Horizontal");
-            if ((Time.time > nextActionTime) && (nextActionTime <= 3))
+            
+            if (TimeToStart >= 0)
             {
-                Numbers.text = nextActionTime.ToString();
-                nextActionTime += period;
-                if (Numbers.text == "3")
+                _gameTime += 1 * Time.deltaTime;
+                if (_gameTime >= 1)
                 {
-                    Numbers.gameObject.SetActive(false);
-                    Title.text = "GO!!!";
-                    Title.transform.position = new Vector3(Title.transform.position.x + 1.85f,
-                        Title.transform.position.y, Title.transform.position.z);
-                    StartGame();
+                    Debug.Log(TimeToStart);
+                    Numbers.text = TimeToStart.ToString();
+                    TimeToStart -= 1;
+                    _gameTime = 0;
                 }
+            }
+
+            if (TimeToStart == -1)
+            {
+                
+                Numbers.gameObject.SetActive(false);
+                Title.text = "GO!!!";
+                Title.transform.position = new Vector3(Title.transform.position.x + 1.85f,
+                    Title.transform.position.y, Title.transform.position.z);
+                TimeToStart--; 
+                StartGame();
             }
 
             if (TimeLeft > 0)
@@ -64,8 +75,7 @@ namespace Game
                         Up.gameObject.SetActive(true);
                         if (_vertical > 0)
                         {
-                            _score += 200;
-                            score.text = "Score: " + _score;
+                            addScore(200);
                             Up.gameObject.SetActive(false);
                             StartGame();
                         }
@@ -74,8 +84,7 @@ namespace Game
                         Down.gameObject.SetActive(true);
                         if (_vertical < 0)
                         {
-                            _score += 200;
-                            score.text = "Score: " + _score;
+                            addScore(200);
                             Down.gameObject.SetActive(false);
                             StartGame();
                         }
@@ -84,8 +93,7 @@ namespace Game
                         Right.gameObject.SetActive(true);
                         if (_horizontal > 0)
                         {
-                            _score += 200;
-                            score.text = "Score: " + _score;
+                            addScore(200);
                             Right.gameObject.SetActive(false);
                             StartGame();
                         }
@@ -94,8 +102,7 @@ namespace Game
                         Left.gameObject.SetActive(true);
                         if (_horizontal < 0)
                         {
-                            _score += 200;
-                            score.text = "Score: " + _score;
+                            addScore(200);
                             Left.gameObject.SetActive(false);
                             StartGame();
                         }
@@ -103,9 +110,14 @@ namespace Game
                 }
             }
 
-            if (TimeLeft == 0)
+            if (TimeLeft == 0)                        //Если закончилось время за которое нужно успеть нажать
             {
-                Debug.Log("Loose");
+                panel.gameObject.SetActive(true);
+                Up.gameObject.SetActive(false);
+                Down.gameObject.SetActive(false);
+                Left.gameObject.SetActive(false);
+                Right.gameObject.SetActive(false);
+                TimeLeft = -1;
             }
         }
 
@@ -115,5 +127,25 @@ namespace Game
             TimeLeft = 2;
         }
         
+        public void addScore(int HowManyScore)
+        {
+            var scoreTag = GameObject.FindGameObjectsWithTag("Score"); 
+            _score += HowManyScore;
+            var scoreText = scoreTag[0].GetComponents<Text>();
+            scoreText[0].text = "Score: " + _score;
+        }
+
+        public void StartAgain()
+        {
+            panel.gameObject.SetActive(false);
+            Numbers.gameObject.SetActive(true);
+            _score = 0;
+            Title.text = "Are you ready?";
+            Title.transform.position = new Vector3(Title.transform.position.x - 1.85f,
+                Title.transform.position.y, Title.transform.position.z);
+            
+            Numbers.text = "3";
+            TimeToStart = 3;
+        }
     }
 }
